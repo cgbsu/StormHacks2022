496a04a271e5e0ce156e2391b5820a5794b057dd
LukasSonntag <LSONNTAG@student.bridgew.edu>
2022-02-19 21:13:37 -0500

    Update VirtualPetGraphics.java
    added deltaTime calculation as well as frame limiting to ~60fps

--- VirtualPetGraphics.java
+++ VirtualPetGraphics.java
@@ -7,11 +7,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Graphics2D;
 import java.awt.event.*;
+import java.time.*;
 // import java.awt.MouseListener;
 
 class VirtualPetGraphics extends JPanel implements MouseListener
 {
     Doggo doggo;
+
+    Clock cluck = Clock.systemDefaultZone();
+    long lastFrameMillis = cluck.millis();
+    public float deltaTime = 0f;
+
+    int frameDelay = 0;
+
 
     public static Color COMMUNISM = Color.RED;
 
@@ -28,9 +36,11 @@ class VirtualPetGraphics extends JPanel implements MouseListener
         setBackground( backroundColor );
         addMouseListener( this );
     }
-    
+
     public void paintComponent( Graphics g )
     {
+        calculateDeltaTime();
+
         super.paintComponent( g );
         g.setColor( backroundColor );
         g.fillRect( 0, 0, getWidth(), getHeight() );
@@ -48,7 +58,36 @@ class VirtualPetGraphics extends JPanel implements MouseListener
         ( ( Graphics2D ) g ).translate( -50, -50 );
         g.fillRect( 200, 200, 100, 100 );*/
 
+        int targetDeltaMilli = 1000 / 60;
+        int deFloatDeltaTime = (int)(deltaTime * 1000);
+
+        if (deFloatDeltaTime > targetDeltaMilli)
+            frameDelay--;
+        else if (deFloatDeltaTime < targetDeltaMilli)
+            frameDelay++;
+
+        try{
+            Thread.sleep(frameDelay);
+        }
+        catch (InterruptedException ex)
+        {
+            System.out.println("damm");
+        }
     }
+
+    void calculateDeltaTime()
+    {
+        long currentMillis = cluck.millis();
+
+        long deltaTimeLong = currentMillis - lastFrameMillis; 
+
+        deltaTime = (float)deltaTimeLong * 0.001f;
+
+        lastFrameMillis = currentMillis;
+
+        System.out.println(deltaTime);
+    }
+
     @Override
     public void mouseClicked( MouseEvent e )
     {
